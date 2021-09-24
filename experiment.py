d353d683a73554ac202fdab36c9b725945c86035
Mustafa Chasmai <mustchasmai@gmail.com>
2021-09-24 18:54:54 +0530

    add opencv KNN model to baseline

--- experiment.py
+++ experiment.py
@@ -26,54 +26,6 @@ def parse_args():
     return args
 
 
-def get_background(prev_frames):
-    back = np.median(np.asarray(prev_frames), axis = 0)
-    return back
-
-def update_prev_frames(prev_frames, cur_img, i):
-    prev_frames[i%k] = cur_img
-    return prev_frames
-
-lower_threshold = (40,40,40)
-higher_threshold = (60, 60, 60)
-
-def threshold(diff):
-    # diff = diff.astype('uint8')
-    # mask = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    mask_confirm = cv2.inRange(diff, higher_threshold, (255,255,255))
-    mask_unsure = cv2.inRange(diff, lower_threshold, higher_threshold)
-
-    for iter in range(50):
-        for i in range(1, diff.shape[0]-1):
-            for j in range(1, diff.shape[1]-1):
-                if mask_unsure[i, j]:
-                    no_sure_neighbours = 0
-                    if mask_confirm[i-1, j]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i+1, j]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i, j+1]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i, j-1]:
-                        no_sure_neighbours += 1
-
-                    if mask_confirm[i-1, j-1]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i+1, j-1]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i+1, j+1]:
-                        no_sure_neighbours += 1
-                    if mask_confirm[i-1, j+1]:
-                        no_sure_neighbours += 1
-
-
-                    if no_sure_neighbours>=1:
-                        mask_confirm[i, j] = 1
-                        mask_unsure[i, j] = 0
-    return mask_confirm
-
-k = 50
-
 def baseline_bgs(args):
 
     os.makedirs(args.out_path, exist_ok=True) 
@@ -81,23 +33,27 @@ def baseline_bgs(args):
         eval_frames_lims = f.read().split(" ")
     eval_frames_lims = [int(x) for x in eval_frames_lims]
 
-    #bg_img = cv2.GaussianBlur(bg_img,(5,5),cv2.BORDER_DEFAULT)
-
-    prev_frames = [cv2.imread(os.path.join(args.inp_path,'in{:06d}.jpg'.format(i))) for i in range(eval_frames_lims[0] - k, eval_frames_lims[0])]
-
     start = timeit.timeit()
 
-    for i in range(eval_frames_lims[0], eval_frames_lims[1] + 1):
+    back_model = cv2.createBackgroundSubtractorKNN(detectShadows = True)
+
+    for i in range(1, eval_frames_lims[1] + 1):
         #print( os.path.join(args.inp_path,'in{:06d}.jpg'.format(i)) )
         #cur_img = cv2.GaussianBlur(cur_img,(5,5),cv2.BORDER_DEFAULT)
 
         cur_img = cv2.imread(os.path.join(args.inp_path,'in{:06d}.jpg'.format(i)))
-        bg_img = get_background(prev_frames)
+        cur_img = cv2.medianBlur(cur_img, 5)
+        mask = back_model.apply(cur_img)
 
-        cur_img = cur_img.astype(float)
-        bg_img = bg_img.astype(float)
-        diff = cv2.absdiff(bg_img, cur_img)
-        mask = threshold(diff)
+        # _, mask = cv2.threshold(mask, 0.5, 1, cv2.THRESH_BINARY)
+
+
+        # print(np.max(mask), np.min(mask))
+        # print(mask)
+        # print(mask.shape)
+
+        if i<eval_frames_lims[0]:
+            continue
 
         Rcontours, hier_r = cv2.findContours(mask,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
         r_areas = [cv2.contourArea(c) for c in Rcontours]
@@ -105,16 +61,16 @@ def baseline_bgs(args):
         CntExternalMask = np.zeros(mask.shape[:2], dtype="uint8")
 
         for c in Rcontours:
-            if(( cv2.contourArea(c) > 20)):
+            if(( cv2.contourArea(c) > 50)):
                 cv2.drawContours(CntExternalMask, [c], -1, 1, -1)
 
         mask = CntExternalMask
+
         kernel = np.ones((5,5), np.uint8)  
         mask = cv2.dilate(mask, kernel, iterations=1)
         mask = cv2.erode(mask, kernel, iterations=1)
 
         cv2.imwrite(args.out_path+'/gt{:06d}.png'.format(i), 255*mask)
-        prev_frames = update_prev_frames(prev_frames, cur_img, i)
         print(i)
 
     end = timeit.timeit()
